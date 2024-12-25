eebdfcfcb7008f3ae719005c3152330523001606
alankriti9399 <60104463+alankriti9399@users.noreply.github.com>
2024-12-25 22:45:50 +0530

    Update main (1).py

--- main (1).py
+++ main (1).py
@@ -7,6 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1u9CqgGjRXmuNLQGlHB2ifwivP6WXzCxn
 """
 
+pip install google-api-python-client
 from googleapiclient.discovery import build
 from google.oauth2.service_account import Credentials
 import pandas as pd
